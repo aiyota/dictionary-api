863a316078bb473e5f3e8c66a3b663b22a0d8ebe
Aaron Y <aiyota@outlook.com>
2022-01-28 11:44:01 -0800

    Add word entity

---
+++ src/words/word.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Word {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  word: string;
+
+  partOfSpeech: string;
+
+  definitions: string[];
+}
