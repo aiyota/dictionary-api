51fbbf81fa5b82bbf01490f93e898434196258c1
Aaron Y <aiyota@outlook.com>
2022-02-01 03:35:53 -0800

    Add delete, patch, and get all parts of speech functionality

--- src/part-of-speech/part-of-speech.controller.ts
+++ src/part-of-speech/part-of-speech.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { PartOfSpeech } from "./part-of-speech.entity";
 import { PartOfSpeechService } from "./part-of-speech.service";
 
@@ -6,10 +14,28 @@ import { PartOfSpeechService } from "./part-of-speech.service";
 export class PartOfSpeechController {
   constructor(private partOfSpeechService: PartOfSpeechService) {}
 
+  @Get()
+  getAllPartsOfSpeech(): Promise<PartOfSpeech[]> {
+    return this.partOfSpeechService.getAllPartsOfSpeech();
+  }
+
   @Post()
   createPartOfSpeech(
     @Body("partOfSpeech") partOfSpeech: string,
   ): Promise<PartOfSpeech> {
     return this.partOfSpeechService.createPartOfSpeech(partOfSpeech);
+  }
+
+  @Patch("/:id")
+  editPartOfSpeech(
+    @Param("id") id: string,
+    @Body("partOfSpeech") partOfSpeech: string,
+  ): Promise<PartOfSpeech> {
+    return this.partOfSpeechService.editPartOfSpeech({ id, partOfSpeech });
+  }
+
+  @Delete("/:id")
+  deletePartOfSpeech(@Param("id") id: string): Promise<PartOfSpeech> {
+    return this.partOfSpeechService.deletePartOfSpeech(id);
   }
 }
--- src/part-of-speech/part-of-speech.service.ts
+++ src/part-of-speech/part-of-speech.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { makeRecord } from "src/utils";
 import { PartOfSpeech } from "./part-of-speech.entity";
 import { PartOfSpeechRepository } from "./part-of-speech.repository";
 
 @Injectable()
 export class PartOfSpeechService {
   constructor(private partOfSpeechRepository: PartOfSpeechRepository) {}
+
+  async getAllPartsOfSpeech(): Promise<PartOfSpeech[]> {
+    return this.partOfSpeechRepository.find();
+  }
 
   async createPartOfSpeech(partOfSpeech: string): Promise<PartOfSpeech> {
     const record = this.partOfSpeechRepository.create({
@@ -14,5 +19,27 @@ export class PartOfSpeechService {
     await this.partOfSpeechRepository.save(record);
 
     return record;
+  }
+
+  async editPartOfSpeech({ id, partOfSpeech }): Promise<PartOfSpeech> {
+    await this.partOfSpeechRepository.update(id, makeRecord({ partOfSpeech }));
+    const editedPartOfSpeech = await this.partOfSpeechRepository.findOne({
+      id,
+    });
+
+    return editedPartOfSpeech;
+  }
+
+  async deletePartOfSpeech(id: string): Promise<PartOfSpeech> {
+    const deletedPartOfSpeech = await this.partOfSpeechRepository.findOne({
+      id,
+    });
+
+    if (!deletedPartOfSpeech)
+      throw new Error(`Part of Speech with id ${id} does not exist`);
+
+    await this.partOfSpeechRepository.delete(id);
+
+    return deletedPartOfSpeech;
   }
 }
