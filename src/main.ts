cd2564cba765ef13b27ae916551f522520759747
Aaron Y <aiyota@outlook.com>
2022-02-08 10:12:05 -0800

    Add helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import config from "config/config";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
   await app.listen(config.app.appPort);
 }
 bootstrap();
