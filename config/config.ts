006cf722eeb3bf9d3a6642c6d633ee5e1568ed56
Aaron Y <aiyota@outlook.com>
2022-02-08 07:41:19 -0800

    Add app port to config

--- config/config.ts
+++ config/config.ts
@@ -6,16 +6,21 @@ ConfigModule.forRoot({
   envFilePath: "./config/.config.env",
 });
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, APP_PORT } =
+  process.env;
 
 assert(DB_HOST, "DB_HOST is not present in .env file");
 assert(DB_PORT, "DB_PORT is not present in .env file");
 assert(DB_USERNAME, "DB_USERNAME is not present in .env file");
 assert(DB_PASSWORD, "DB_PASSWORD is not present in .env file");
 assert(DB_DATABASE, "DB_DATABASE is not present in .env file");
+assert(APP_PORT, "DB_DATABASE is not present in .env file");
 
 type Config = {
   db: TypeOrmModuleOptions;
+  app: {
+    appPort: string;
+  };
 };
 
 const db: TypeOrmModuleOptions = Object.freeze({
@@ -29,8 +34,13 @@ const db: TypeOrmModuleOptions = Object.freeze({
   synchronize: true,
 });
 
+const app = {
+  appPort: APP_PORT,
+};
+
 const config: Config = Object.freeze({
   db,
+  app,
 });
 
 export default config;
